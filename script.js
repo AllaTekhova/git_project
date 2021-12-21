75d1fa50c57e67ca095c18c94a51e8586b746cf2
Alla Tekhova <alla.ua.lazareva@gmail.com>
2021-12-21 20:34:12 +0200

    empty file

--- script.js
+++ script.js
@@ -1,28 +1,2 @@
 "use strict"
 
-let numb = 50;
-
-// while(numb <= 55) {
-//    console.log(numb);
-//    numb++;
-// }
-
-// do {
-//    console.log(numb);
-//    numb++;
-// }
-// while(numb < 55);
-
-for(let i = 1; i < 8; i++) {
-   if(i === 6) {
-      break; // доходит до 6 и останавливается, не включая 6 в список и остонавливает выполнения условия
-   }
-   console.log(i);
-}
-
-for(let i = 1; i < 8; i++) {
-   if(i === 6) {
-      continue;  // пропускает 6 и продолжает пока не выполнится условие
-   }
-   console.log(i);
-}
